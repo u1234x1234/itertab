0c5be1cabaf4cb8d756255069c8c930742169083
u1234x1234 <u1234x1234@gmail.com>
2018-07-20 03:55:30 +0500

    accept dict & predict direction

--- itertab/itertab.py
+++ itertab/itertab.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import atexit
 import os
+import re
 import sys
 
 import numpy as np
@@ -11,41 +12,37 @@ from tabulate import _format_table as default_format
 
 
 TERMINAL = Terminal()
+ASC_PATTERN = re.compile('(Acc)|(Prec)', re.I)
+DESC_PATTERN = re.compile('(Logloss)|(entropy)|(ce)', re.I)
 
 
-def _format_table(fmt, headers, rows, colwidths, colaligns, is_multiline):
-    n_columns = len(rows[0])
-    n_lines = len(rows)
+def predict_target_direction(name):
+    if ASC_PATTERN.findall(name):
+        return 1
+    if DESC_PATTERN.findall(name):
+        return -1
+    return 0
 
-    modified_rows = []
-    for col_idx in range(n_columns):
-        col = [v[col_idx] for v in rows]
-        try:
-            float_column = np.array(col, dtype=np.float32)
-        except ValueError as e:
-            modified_rows.append(col)
-            continue
 
-        m_row = [col[0]]
-        for i in range(1, len(float_column)):
-            if float_column[i] > float_column[i - 1]:  # TODO ==
-                m_row.append(Fore.GREEN + col[i] + Style.RESET_ALL)
+def flatten_dict(d):
+    def items():
+        for key, value in d.items():
+            if isinstance(value, dict):
+                for subkey, subvalue in flatten_dict(value).items():
+                    yield key + '_' + subkey, subvalue
             else:
-                m_row.append(Fore.RED + col[i] + Style.RESET_ALL)
-        modified_rows.append(m_row)
-    rows = list(map(list, zip(*modified_rows)))
-    r = default_format(fmt, headers, rows, colwidths, colaligns, is_multiline)
+                yield key, value
 
-    return r
+    return dict(items())
 
 
 class Table:
-    def __init__(self, columns):
-        self.columns = columns
+    def __init__(self):
+        self.columns = None
         self.rows = []
         self.prev = ''
         atexit.register(self.cleanup)
-        tabulate._format_table = _format_table
+        tabulate._format_table = self._format_table
         self.default_stdout_write = sys.stdout.write
         sys.stdout.write = self.patched_write
 
@@ -57,7 +54,42 @@ class Table:
         else:
             self.default_stdout_write(q)
 
+    def _format_table(self, fmt, headers, rows, colwidths, colaligns, is_multiline):
+        n_columns = len(rows[0])
+        n_lines = len(rows)
+
+        modified_rows = []
+        for col_idx in range(n_columns):
+            col = [v[col_idx] for v in rows]
+            try:
+                float_column = np.array(col, dtype=np.float32)
+            except ValueError as e:
+                modified_rows.append(col)
+                continue
+
+            m_row = [col[0]]
+            for i in range(1, len(float_column)):
+                is_gt = float_column[i] > float_column[i - 1]
+                direction = predict_target_direction(self.columns[col_idx])
+                if (is_gt and direction == 1) or (not is_gt and direction == -1):  # TODO ==
+                    color = Fore.GREEN
+                else:
+                    color = Fore.RED
+                if direction == 0:
+                    color = Fore.WHITE
+
+                m_row.append(color + col[i] + Style.RESET_ALL)
+            modified_rows.append(m_row)
+        rows = list(map(list, zip(*modified_rows)))
+        r = default_format(fmt, headers, rows, colwidths, colaligns, is_multiline)
+
+        return r
+
     def add_row(self, row):
+        row = flatten_dict(row)
+        if self.columns is None:
+            self.columns = list(row.keys())
+        row = row.values()
         if len(row) != len(self.columns):
             raise Exception('Number of columns mismatch: {} != {}'.format(len(self.columns), len(row)))
 
